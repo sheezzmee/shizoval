a1abb389279a343030a44548a745f815a86e1e4c
sheezzmee <106715792+sheezzmee@users.noreply.github.com>
2024-01-06 19:03:25 +0300

    Create shizoval.user.js

---
+++ shizoval.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name             shizoval
+// @author           sheezzmee
+// @version          ооочень бета 
+
+// @author-discord   sheezzmee, https://discord.gg/gMAJX2AdBS
+
+// @match            https://*.test-eu.tankionline.com/browser-public/index.html?*
+// @match            https://tankionline.com/play*
+
+// @grant            none
+// @icon             https://www.google.com/s2/favicons?sz=64&domain=tankionline.com
+// @run-at           document-start
+// ==/UserScript==
+
+// скоро
